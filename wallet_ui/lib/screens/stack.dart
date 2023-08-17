800931b543c8b8a3852a25ab621642a1b4128168
Serigne Saliou Mbacké NDIAYE <68548037+Saliou96@users.noreply.github.com>
2023-08-17 08:46:58 +0000

    add

---
+++ wallet_ui/lib/screens/stack.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Stack extends StatelessWidget {
+  const Stack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // drawer: Drawer(),
+      body: Stack(
+
+      ),
+    );
+  }
+}
