0f59a280b1a1f729717b244c27002b35e1930f6e
Serigne Saliou Mbacké NDIAYE <68548037+Saliou96@users.noreply.github.com>
2023-08-17 18:27:51 +0000

    add

--- wallet_ui/lib/screens/login.dart
+++ wallet_ui/lib/screens/login.dart
@@ -6,52 +6,71 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Stack(
-        alignment: Alignment.bottomCenter,
+        body: Column(
+      children: [
+        Row(
           children: [
-            Container(
-              height:500,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.orange,
-              ),
-            ),
-            Container(
-              height: 490,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.green
-              ),
-            ),
-            Container(
-              height: 480,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset('assets/images/logo.png')
-                      ],
+            Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: 500,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.orange,
                     ),
-                    Row(
-                      children:[
-                        Text("SAVE MANGO")
-                      ]                  
-                    )
-                  ],
-                ),
+                  ),
+                  Container(
+                    height: 490,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.green),
+                  ),
+                  Container(
+                    height: 480,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(children: [
+                            ElevatedButton(
+                              child: Text("SIGN IN"),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: Size(250, 40),
+                                  primary: Colors.orange,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30))),
+                            )
+                          ]),
+                          SizedBox(height: 20),
+                          Row(children: [
+                            ElevatedButton(
+                              child: Text("SIGN UP"),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: Size(250, 40),
+                                  primary: Colors.orange,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30))),
+                            )
+                          ]),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
           ],
         ),
-      ),
-    );
+        Row()
+      ],
+    ));
   }
 }
