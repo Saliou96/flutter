ec0d8ba9b4bcf05a4988f49ff2b98e31858a000c
unknown <serignesaliou993@gmail.com>
2024-07-26 18:39:13 +0000

    reset

--- auth_register_ui/android/app/src/main/kotlin/com/example/auth_register_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.auth_register_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- todo_ca/android/app/src/main/kotlin/com/example/todo_ca/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.todo_ca
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
